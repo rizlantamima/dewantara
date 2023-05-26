e5d5e5264bb13f17ef38abc4d14a326e35237845
Rizlan Nawfal Tamima <rtamima@gmail.com>
2023-05-26 22:34:36 +0700

    f this shit im out

--- main.go
+++ main.go
@@ -2,28 +2,25 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"strconv"
 
-	"github.com/rizlantamima/dewantara/handler"
+	"github.com/labstack/echo/v4"
 )
 
 func main()  {
 	listenAddressPort := flag.Int("port",3000,"port to serve HTTP on")
 	flag.Parse()
 
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handler.HandleWellcome)
-	mux.HandleFunc("/users", handler.HandleUsers)
-	mux.HandleFunc("/users/", handler.HandleUserDetail)	
+	// mux.HandleFunc("/", handler.HandleWellcome)
+	// mux.HandleFunc("/users", handler.HandleUsers)
+	// mux.HandleFunc("/users/", handler.HandleUserDetail)	
 
 
 	listenAddressPortString := ":" + strconv.Itoa(*listenAddressPort)
 
-	fmt.Printf("Your services are runing on port : %v",*listenAddressPort)
-	log.Fatal(http.ListenAndServe(listenAddressPortString, mux))
+	app := echo.New()
+	app.Logger.Fatal(app.Start(listenAddressPortString))
 
 }
